c4967ff2f5e4ca3e988a07a62b7726418d8c0b7d
dentler <dentler@dentler-Latitude-E5440jan.dentler@uni.lu>
2018-10-29 09:02:38 +0100

    Import issue solutions from https://github.com/DentOpt/denmpc

--- src/user_scenarios/ardrone_sensor_tracking.cpp
+++ src/user_scenarios/ardrone_sensor_tracking.cpp
@@ -39,20 +39,12 @@ ros::init(argc, argv, "controller");
 			0.0,0.0,0.0, 0.0,0.0, 0.0,0.0,0.0,0.0, //State Penalty: no tracking
 			1.0,1.0,0.3,1.0 //Input penalty
 	};
-	double ardrone0_init_x[]=   {0,0,2, 1,0, 0.0,0.0,0.0,0.0};
-	double ardrone0_init_xdes[]={0,0,2, 1,0, 0.0,0.0,0.0,0.0};
-	ardrone0->setInitialState(ardrone0_init_x);
-	ardrone0->setInitialDesiredState(ardrone0_init_xdes);
 	ardrone0->setInitialParameter(ardrone0_init_p);
-	// ardrone0->setStateSubscriberRosTopicName       ("/Ardrone2SimpleLinModel_HASHMARK_0/pose");
-	// ardrone0->setDesiredStateSubscriberRosTopicName("/Ardrone2SimpleLinModel_HASHMARK_0/desiredpose");
-	// ardrone0->setPublisherRosTopicName             ("/Ardrone2SimpleLinModel_HASHMARK_0/ext_ctrl_ch");
 	agentlist.push_back(ardrone0); /*add to agentlist*/
 
 	//AddConstraint
 	std::vector<Controller*> constraintlist;
 	Constraint* constraint= new OrientationConstraint_20170227(ardrone0);
-
 	//constraint_init_p{k0, ds, beta, ddist, kforw, kside, kup}
 	double constraint0_init_p[]={1,0.17,0.5,1.5,1,1,1};
 	constraint->setInitialParameter(constraint0_init_p);
@@ -63,14 +55,6 @@ ros::init(argc, argv, "controller");
 
 	//Initialize: Controller
 	Cmscgmres* controller1=new Cmscgmres(agentlist,controllerlist.size());
-	// controller1->setHorizonDiskretization(10);
-	// controller1->setHorizonLength(1);
-	// controller1->setTolerance(1e-8);
-	// controller1->setUpdateIntervall(0.01);
-	// controller1->setMaximumNumberofIterations(10);
-	// controller1->activateInfo_ControllerTrace();
-	// controller1->activateInfo_Controller();
-	// controller1->startLogging2File();
 	controller1->activateInfo_ControllerStates();
 	controllerlist.push_back(controller1);
 
@@ -81,9 +65,6 @@ ros::init(argc, argv, "controller");
 /****** Initialize Scheduler ******/
 	Scheduler scheduler(argc,argv,controllerlist, eventlist);
 	scheduler.run_control(0.01);
-//	scheduler.run_vrep(0.01);
-//	scheduler.run_simulation(0.1,10);
-
 };
 
 
